6373ee48647999467595dd4785e525d045f17e89
NikhilTheSlayer <nikhilkajota9413750125@gmail.com>
2024-05-22 01:01:03 +0530

    env commit

--- server/db.js
+++ server/db.js
@@ -1,15 +1,14 @@
-// db.js (Assuming pool is exported among other things)
 import pg from 'pg';
+import dotenv from 'dotenv';
 
-
+dotenv.config();
 
 const pool = new pg.Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "students",
-  password: "12345",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
-export default pool
-
+export default pool;
